77039a3a49935433f6b4e32d2433a2d197122f58
DESKTOP-UHJ0L34\marki <marki.marketa@gmail.com>
2021-04-13 19:27:24 +0200

    úkol

--- main.js
+++ main.js
@@ -5,9 +5,10 @@
 // První příklad - vypiš vzestupně čísla od 0 do 10
 console.log('První příklad - vzestupná řada 0 - 10');
 
-//TADY NAPIS CYKLUS
+for (let i=0; i<=10; i=i+1) {
+    console.log(i)
+}
 
-console.log('-------------------');
 
 
 
@@ -17,15 +18,15 @@ console.log('-------------------');
 
 console.log('Druhý příklad - rozdíl mezi <= a <');
 
-//TADY NAPIS CYKLUS
-
-console.log('-------------------');
+for (let i=0; i<10; i=i+1) {
+    console.log(i)
+}
 
 
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
-//TADY NAPIS CYKLUS
-
-console.log('-------------------');
+for (let i=10; i>=0; i=i-1) {
+    console.log(i)
+}
